9a4b0002c38e8483f80719c9277a715d945b5d40
Bozzetto Mirko <bozzettomirko@M1.lan>
2023-04-24 21:44:39 +0200

    configuration de firebase

---
+++ front-end/src/config/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDhtziWnHgsPA8cKktx-UsDYO_x2Y5Ym9A",
+  authDomain: "m1rk0-nextjs-sandbox.firebaseapp.com",
+  projectId: "m1rk0-nextjs-sandbox",
+  storageBucket: "m1rk0-nextjs-sandbox.appspot.com",
+  messagingSenderId: "537849532433",
+  appId: "1:537849532433:web:3d59fb98a47799caa5a1b0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
