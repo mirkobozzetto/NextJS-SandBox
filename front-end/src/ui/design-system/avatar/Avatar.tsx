33c3c753eca2e0bdec6907ad50303c9b025a2dbc
Bozzetto Mirko <bozzettomirko@M1.lan>
2023-05-02 19:50:34 +0200

    setup avantars ok

---
+++ front-end/src/ui/design-system/avatar/Avatar.tsx
@@ -0,0 +1,34 @@
+import clsx from "clsx";
+import Image from "next/image";
+
+interface Props {
+  size?: "small" | "medium" | "large";
+  src: string;
+  alt: string;
+}
+
+export const Avatar = ({ size = "medium", src, alt }: Props) => {
+  let sizeStyles: string;
+
+  switch (size) {
+    case "small":
+      sizeStyles = "w-[24px] h-[24px]";
+      break;
+    case "medium": // Default
+      sizeStyles = "w-[34px] h-[34px]";
+      break;
+    case "large":
+      sizeStyles = "w-[50px] h-[50px]";
+      break;
+  }
+  return (
+    <div className={clsx(sizeStyles, "bg-gray-400 rounded-full relative ")}>
+      <Image
+        fill
+        src={src}
+        alt="Avatar Mirko"
+        className="object-cover object-center rounded-full"
+      />
+    </div>
+  );
+};
